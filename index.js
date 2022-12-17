d296869267ef7bd41609796ed8b221ee79abc1a3
GeethMalinda <kdgmwijesekara22@gmail.com>
2022-12-17 08:42:52 +0530

    body parser

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const connection =  require('./connection');
-
+const userRoute = require('./routes/user.js');
+const bodyParser = require("body-parser");
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
-app.use(express.json);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/user',userRoute);
 
 module.exports = app;
