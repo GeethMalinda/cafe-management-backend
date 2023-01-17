ab39ab244e6a763753dadd93ade8a0d300916f7e
Geeth <kdgmwijesekara22@gmail.com>
2023-01-17 06:10:18 +0530

    product update and delete

--- routes/product.js
+++ routes/product.js
@@ -50,4 +50,34 @@ router.get('getById/:id',auth.authenticationToken,(req, res) => {
     }))
 })
 
+router.patch('/update',auth.authenticationToken,checkRole.checkRole,(req, res) => {
+    let product = req.body;
+    let query = "update product set name = ?,categoryId = ?,description = ?,price = ? where id = ? ";
+    connection.query(query,[product.name,product.categoryId,product.description,product.price,product.id],(err, result) => {
+        if (!err){
+            if (result.affectedRows === 0){
+                return res.status(404).json({message:"Product Id Does Not Exists"})
+            }
+            return res.status(200).json({message:"Product Updated Successfully"})
+        }else {
+            return res.status(500).json(err)
+        }
+    })
+})
+
+router.delete('/delete/:id',auth.authenticationToken,checkRole.checkRole,(req, res) => {
+    const id = req.params.id;
+    let query = "delete from product where id = ?";
+    connection.query(query,[id],((err, result) => {
+        if (!err) {
+            if (result.affectedRows === 0) {
+                return res.status(404).json({message:'Product Id does not found'})
+            }
+            return res.status(200).json({message:"Product Deleted Successfully"});
+        }else {
+            return res.status(500).json(err);
+        }
+    }))
+})
+
 module.exports = router;
